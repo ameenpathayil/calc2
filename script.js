3f908860654d15f3760498df2e40555ca9c0cb1a
mohammed ameen pathayil <57120113+ameenpathayil@users.noreply.github.com>
2022-09-06 14:29:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,124 @@
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector("#value");
+const tempResultEl = document.querySelector(".temp-result");
+const numbersEl = document.querySelectorAll(".numbers");
+const operationEl = document.querySelectorAll(".operation");
+const equalEl = document.querySelector("#equal");
+const clearAllEl = document.querySelector("#del");
+const clearLastEl = document.querySelector(".last-entity-clear");
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
 
-function clickButton(val){
-    console.log(val)
-    document.getElementById("result-screen").value=document.getElementById("result-screen").value +val;
+numbersEl.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    dis2Num += e.target.innerText;
+    display2El.value = dis2Num;
+    // console.log();
+  });
+});
 
+operationEl.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!dis2Num) return;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (dis1Num && dis2Num && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(dis2Num);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+    console.log(result);
+  });
+});
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " ";
+  display2El.value = "";
+  dis2Num = "";
+  display2El.value = result;
 }
 
-function clearScreen(){
-    document.getElementById("result-screen").value="";
+function mathOperation() {
+  if (lastOperation === "x") {
+    result = parseFloat(result) * parseFloat(dis2Num);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(dis2Num);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(dis2Num);
+  } else if (lastOperation === "%") {
+    result = parseFloat(result) % parseFloat(dis2Num);
+  }
 }
+// operation();
 
-function equalClick(){
-    var text=document.getElementById("result-screen").value;
-    var result=eval(text)
-    document.getElementById("result-screen").value=result;
+equalEl.addEventListener("click", () => {
+  if (!dis2Num || !dis1Num) return;
+  haveDot = false;
+  mathOperation();
+  clearVar();
+  display2El.value = result;
+  dis2Num = result;
+  dis1Num = "";
+});
 
+clearAllEl.addEventListener("click", () => {
+  dis1Num = "";
+  dis2Num = "";
+  display2El.value = "0";
+  result = "";
+});
+
+window.addEventListener("keydown", (e) => {
+  if (
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9" ||
+    e.key === "."
+  ) {
+    clickButtonEl(e.key);
+    // console.log(e.key)
+  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
+    clickOperation(e.key);
+  } else if (e.key === "*") {
+    clickOperation("x");
+    // console.log(e.key)
+  } else if (e.key == "Enter" || e.key === "=") {
+    clickEqual();
+  }
+  // console.log(e.key)
+});
+function clickButtonEl(key) {
+  numbersEl.forEach((button) => {
+    if (button.innerText === key) {
+      button.click();
+    }
+  });
+}
+function clickOperation(key) {
+  operationEl.forEach((operation) => {
+    if (operation.innerText === key) {
+      operation.click();
+    }
+  });
+}
+function clickEqual() {
+  equalEl.click();
 }
